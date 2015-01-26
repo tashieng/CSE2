8c24d84003d928e06c9998b7357b1a9a9d80915d
tashieng <tbe218@lehigh.edu>
2015-01-26 00:28:47 +0000

    updated Welcome

---
+++ hw01/Welcome.java
@@ -0,0 +1,25 @@
+//Tashi Eng
+//WelcomeClass Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//define a class
+public class Welcome{
+    
+//  add main method
+    public static void main(String[] args) {
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-T--B--E--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("This August my wife and I took an eleven-day, 675-mile bike tour on our tandem.  We circled Lake Ontario.");
+
+        }
+    }
