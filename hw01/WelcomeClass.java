f1dd66a392855f887f9dff24faf8a743a40a6b2c
tashieng <tbe218@lehigh.edu>
2015-01-29 01:57:18 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//Tashi Eng
+//WelcomeClass Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//define a class
+public class Welcome{
+    
+//  add main method
+    public static void main(String[] args) {
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-T--B--E--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("This weekend I went to New York City and had sushi for the first time. I then saw my first show on Broadway. It was Aladdin.");
+
+        }
+    }
